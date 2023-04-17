854d58d38c0b6850e5d4646919c3705b705a9ff9
QKD90 <qrenchina@163.com>
2023-04-17 10:21:50 +0800

    配置了逻辑删除和填充策略

--- src/main/java/com/example/mybatisplus/config/MyBatisPlusConfig.java
+++ src/main/java/com/example/mybatisplus/config/MyBatisPlusConfig.java
@@ -1,34 +1,25 @@
 package com.example.mybatisplus.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 // 扫描我们的 mapper 文件夹
 @MapperScan("com.kuang.mapper")
 @EnableTransactionManagement
-@Configuration // 配置类
+@Configuration
 public class MyBatisPlusConfig {
 
-    // 注册乐观锁插件
     @Bean
-    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-        return new OptimisticLockerInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
     }
-
-    // 分页插件
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return  new PaginationInterceptor();
-    }
-
-    // 逻辑删除组件！
-    @Bean
-    public ISqlInjector sqlInjector() {
-        return new LogicSqlInjector();
-    }
-
 
 }
